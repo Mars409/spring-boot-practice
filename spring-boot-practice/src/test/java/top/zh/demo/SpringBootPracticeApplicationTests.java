c447750826d78b75ef2a35420c85a22054b78a83
Mars409 <2081765286@qq.com>
2024-09-03 17:35:41 +0800

    Add files via upload

---
+++ spring-boot-practice/src/test/java/top/zh/demo/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
