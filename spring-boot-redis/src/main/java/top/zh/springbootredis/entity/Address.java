50a640c26e614a514dcc24a28a12c4b173946922
Mars409 <2081765286@qq.com>
2024-11-19 09:16:46 +0800

    Add files via upload

---
+++ spring-boot-redis/src/main/java/top/zh/springbootredis/entity/Address.java
@@ -0,0 +1,20 @@
+package top.zh.springbootredis.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Address implements Serializable {
+    @Serial
+    private static final long serialVersionUID=3781546185454865110L;
+    private String province;
+    private String city;
+}
